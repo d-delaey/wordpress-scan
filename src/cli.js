e64ecbd06e800af6871f77211d43cd5bc506cde7
Delaey <delaey@Delaey>
2021-04-24 17:16:49 +0200

    init

--- src/cli.js
+++ src/cli.js
@@ -1,74 +1,54 @@
-import arg from "arg";
-import inquirer from "inquirer";
+const arg = require("arg");
+const inquirer = require("inquirer");
 
 import Scanner from "./scanner";
+
+export async function cli(args) {
+    let options = await parseArguments(args);
+
+    let scanner = new Scanner(options.sshCredentials, options.localPath);
+
+    scanner.start();
+}
 
 async function parseArguments(rawArgs) {
     const args = arg(
         {
+            "--download": Boolean,
             "--host": String,
             "--username": String,
             "--password": String,
             "--port": Number,
-            "--key": String,
-            "--passphrase": String,
+            "--wordpressRoot": String,
+
+            "--path": String,
         },
         {
             argv: rawArgs.slice(2),
         }
     );
-    let options = {
-        host: args["--host"] || false,
-        username: args["--username"] || false,
-        password: args["--password"] || false,
-        port: args["--port"] || 22,
-        privateKey: args["--key"] || false,
-        passphrase: args["--passphrase"] || false,
-    };
+    let options = {};
+    options.downloadFiles = false;
+    options.sshCredentials = {};
+    options.localPath = args["--path"];
 
-    if (!options.host || !options.username) {
-        throw new Error("missing required arguments");
+    if (!options.localPath && !args["--download"]) {
+        throw "Missing required Parameter";
     }
 
-    /* If Password is not passed as a Argument start a prompt to fill in the password */
-    if (!options.password && !options.privateKey) {
-        await inquirer
-            .prompt({
-                type: "input",
-                name: "password",
-                message: "Enter SSH Pasword:",
-            })
-            .then((password) => {
-                options.password = password.password;
-            });
-    }
+    if (args["--download"]) {
+        options.downloadFiles = true;
 
-    if (options.privateKey && !options.passphrase) {
-        await inquirer
-            .prompt({
-                type: "input",
-                name: "passphrase",
-                message: "Enter Passphrase Password:",
-            })
-            .then((passphrase) => {
-                options.passphrase = passphrase.passphrase;
-            });
+        options.sshCredentials.host = args["--host"] || false;
+        options.sshCredentials.username = args["--username"] || false;
+        options.sshCredentials.password = args["--password"] || false;
+        options.sshCredentials.port = args["--port"] || 22;
+        options.sshCredentials.wordpressRoot = args["--wordpressRoot"] || false;
+
+        if (!options.sshCredentials.host || !options.sshCredentials.username || !options.sshCredentials.password) {
+            throw "Missing required Parameter";
+        }
     }
 
     return options;
-}
-
-export async function cli(args) {
-    let options = await parseArguments(args);
-
-    let scanner = new Scanner(
-        options.host,
-        options.username,
-        options.password,
-        options.privateKey,
-        options.passphrase,
-        options.port
-    );
-
-    scanner.connect();
 }
--- src/scanner.js
+++ src/scanner.js
@@ -1,73 +1,139 @@
-const { NodeSSH } = require("node-ssh");
-const fs = require('fs');
+const {NodeSSH} = require("node-ssh");
+const fs = require("fs");
+const path = require("path");
+const axios = require("axios");
+const readdirp = require("readdirp");
+const crypto = require("crypto");
 
 class Scanner {
-    constructor(host, username, password, key, passphrase, port = 22, wordpressRoot = null) {
-        this.host = host;
-        this.username = username;
-        this.port = port;
-        this.password = password;
-        this.privateKey = key;
-        this.passphrase = passphrase;
-        this.wordpressRoot = "/var/www/vhosts/delaey.space/httpdocs";
-        this.changeToWordpressRoot = 'cd ' + this.wordpressRoot + ' && '; 
-        this.wordpressVersion = null;
+    constructor(credentials, localPath) {
+        this.credentials = credentials;
+
+        this.localPath = localPath;
     }
 
-    getConnection() {
-        return this.ssh.getConnection();
-    }
-
-    async connect() {
-        if (!this.ssh) this.ssh = new NodeSSH();
-
-        let args = {
-            host: this.host,
-            username: this.username,
-            port: this.port,
-        };
-
-        if (this.privateKey && this.passphrase) {
-            args.privateKey = this.privateKey;
-            args.passphrase = this.passphrase;
+    async start() {
+        if (Object.keys(this.credentials).length !== 0) {
+            await this.downloadFiles();
         }
 
-        await this.ssh.connect(args);
+        this.compareChecksums();
+    }
 
-        this.getWordPressVersion();
+    async downloadFiles() {
+        if (!this.ssh) this.ssh = new NodeSSH();
+
+        let wordpressRootCommand = "cd " + this.credentials.wordpressRoot + " && ";
+
+        if (!this.ssh.isConnected()) {
+            let args = {
+                host: this.credentials.host,
+                username: this.credentials.username,
+                port: this.credentials.port,
+                password: this.credentials.password,
+            };
+
+            await this.ssh.connect(args);
+        }
+
+        await this.ssh.getDirectory(this.createDownloadPath(), this.credentials.wordpressRoot, {
+            recursive: true,
+            concurrency: 10,
+        });
+
+        this.ssh.dispose();
+    }
+
+    createDownloadPath() {
+        if (this.localPath) return this.localPath;
+
+        let path = __dirname.split("/");
+        path.pop();
+        this.localPath = path.join("/") + "/scans/" + this.credentials.host + "." + this.credentials.username;
+        if (!fs.existsSync(this.localPath)) {
+            fs.mkdirSync(this.localPath);
+        }
+        return this.localPath;
     }
 
     getWordPressVersion() {
-        if(this.wordpressVersion) return this.wordpressVersion;
+        if (this.wordpressVersion) return this.wordpressVersion;
 
-        if(!this.ssh.isConnected) this.connect;
+        let versionFile = this.localPath + "/wp-includes/version.php";
+        let data = fs.readFileSync(versionFile, "utf8");
 
-        this.ssh.execCommand(this.changeToWordpressRoot + 'cat ./wp-includes/version.php', [], { cwd: this.wordpressRoot }).then( (result) => {
-            if(!result.stdout || result.stderror) {
-                console.log("Error: Cannot Find WordPress Version");
-            }
-            let fileContent = result.stdout.split('\n')
-            let versionLine = fileContent.find( element => element.includes('wp_version ='));
-            let wordpressVersion = versionLine.match(/\'([^\']+)\'/)[1];
-            this.wordpressVersion = wordpressVersion;
-        });
+        /* @TODO make this better :D */
+        // this splits the version.php file into an array where each line is a entry
+        // its match the wordpress version between ' ' and then replace ' with nothing so
+        // there is only the version left
+        this.wordpressVersion = data
+            .split("\n")[15]
+            .match(/\'(.*)\'/, "$1")[0]
+            .replaceAll("'", "");
 
         return this.wordpressVersion;
     }
 
-    getOfficialWordpressChecksums() {
+    async getCoreChecksums() {
+        var checksums = null;
 
+        await axios.get("https://api.wordpress.org/core/checksums/1.0/?version=" + this.getWordPressVersion()).then((response) => {
+            checksums = response.data.checksums[this.getWordPressVersion()];
+        });
+
+        return checksums;
     }
 
-    getCurrentWordpressChecksums() {
+    async getPluginChecksums(slug, version) {
+        var checksums = null;
 
+        await axios.get("https://api.wordpress.org/plugin-checksums/" + slug + "/" + version + ".json").then((response) => {
+            checksums = response.data.checksums[this.getWordPressVersion()];
+        });
+
+        return checksums;
     }
 
-    compareChecksums() {
+    async compareCoreChecksums() {
+        let coreChecksums = await this.getCoreChecksums();
+        let failedFiles = [];
 
+        /* Loop Through every file inside Wordpress Folder except wp-content */
+
+        for await (const file of readdirp(this.localPath, {entryType: "all"})) {
+            if (fs.lstatSync(file.fullPath).isDirectory()) continue;
+            let checksum = await this.getFileHash(file.fullPath);
+
+            let coreChecksum = coreChecksum[file.path];
+
+            if (!officialChecksum || checksum !== officialChecksum.replaceAll("\\")) {
+                failedFiles.push(file.path);
+            }
+        }
+
+        return failedFiles;
     }
 
+    async comparePluginChecksums() {}
 
+    getFileHash(filename) {
+        return new Promise((resolve, reject) => {
+            let shasum = crypto.createHash("md5");
+            try {
+                let s = fs.ReadStream(filename);
+                s.on("data", function (data) {
+                    shasum.update(data);
+                });
+                s.on("end", function () {
+                    const hash = shasum.digest("hex");
+                    return resolve(hash);
+                });
+            } catch (error) {
+                console.log("blub");
+                return reject("calc fail");
+            }
+        });
+    }
 }
 
 module.exports = Scanner;
